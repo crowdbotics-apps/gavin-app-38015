b487ca75d09458baf06ddd98fd4bb3dc41cb3e08
Crowdbotics <team@crowdbotics.com>
2022-12-26 17:54:41 +0000

    Update admin title

--- backend/gavin_app_38015/urls.py
+++ backend/gavin_app_38015/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Gavin app"
-admin.site.site_title = "Gavin app Admin Portal"
-admin.site.index_title = "Gavin app Admin"
+admin.site.site_header = "Gavins app"
+admin.site.site_title = "Gavins app Admin Portal"
+admin.site.index_title = "Gavins app Admin"
 
 # swagger
 api_info = openapi.Info(
